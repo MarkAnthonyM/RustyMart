0a82ecd827bf62fab4f4ab155bdd5528b8a9565e
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-10-17 14:13:53 -0500

    Add test logic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
 use yew::prelude::*;
 
+struct Hello {}
+
+impl Component for Hello {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, _: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _: Self::Properties) -> ShouldRender {
+        true
+    }
+
+    fn view(&self) -> Html {
+        html! { <span>{"Hello, World!"}</span> }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Hello>();
 }
