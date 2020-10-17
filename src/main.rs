b6a3b754aa60dcf2bab1695c75e660a9c508ad89
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-10-17 17:08:28 -0500

    Refactor main.rs to use Home module

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,8 @@
+mod pages;
+
+use pages::Home;
 use yew::prelude::*;
 
-struct Hello {}
-
-impl Component for Hello {
-    type Message = ();
-    type Properties = ();
-
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Self {}
-    }
-
-    fn update(&mut self, _: Self::Message) -> ShouldRender {
-        true
-    }
-
-    fn change(&mut self, _: Self::Properties) -> ShouldRender {
-        true
-    }
-
-    fn view(&self) -> Html {
-        html! { <span>{"Hello, World!"}</span> }
-    }
-}
-
 fn main() {
-    yew::start_app::<Hello>();
+    yew::start_app::<Home>();
 }
