af8e98443aef43b9d1afadd29bfdcbd9510cb4c6
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-10-17 18:00:19 -0500

    Change product image links

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -14,14 +14,14 @@ impl Component for Home {
                 id: 1,
                 name: "Apple".to_string(),
                 description: "An apple a day keeps the doctor away".to_string(),
-                image: "/products/apple.jpg".to_string(),
+                image: "https://p1.hiclipart.com/preview/616/544/999/fruit-and-vegetable-red-apple-fruit-png-clipart.jpg".to_string(),
                 price: 3.65,
             },
             Product {
                 id: 2,
                 name: "Banana".to_string(),
                 description: "An old banana leaf was once young and green".to_string(),
-                image: "/products/banana.png".to_string(),
+                image: "https://cdn.iconscout.com/icon/free/png-256/bananas-55-1176320.png".to_string(),
                 price: 7.99,
             },
         ];
